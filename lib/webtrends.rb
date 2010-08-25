3f964fb6dba8c36329598d9717f5358694c71950
John Beynon <john@beynon.org.uk>
2010-08-25 16:17:41 +0100

    Removing webtrends activerecord

--- lib/webtrends.rb
+++ lib/webtrends.rb
@@ -1,15 +1,2 @@
-require 'webtrends/wtgateway'
 require 'webtrends/period'
-
-class Webtrends < ActiveRecord::Base
-  
-  def self.get(period)
-    record = self.find_by_period_label(period)
-    record.nil? ? get_new_period_from_api(period) : record
-  end
-  
-  def self.get_from_db(period)
-    self.find_by_period_label(period)
-  end
-  
-end
+require 'webtrends/wtgateway'
---
+++ test/unit/period_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PeriodTest < ActiveSupport::TestCase
+  
+  context "A webtrends period identifer" do
+  
+  end
+  
+end
